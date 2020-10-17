b71622e42ba7e3114ee30bda645cace72f3826fa
plagueevgeny <PlagueEvgeny@yandex.ru>
2020-10-17 13:44:14 +0500

    django

--- mydjangoapp/mydjangoapp/settings.py
+++ mydjangoapp/mydjangoapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'tw-sr%+6)s^9rkzi7y$k_=@k4mg^#s$f8vm!1mmgs1z=bsum@t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainapp'
 ]
 
 MIDDLEWARE = [
